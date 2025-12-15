ef81151591e3d3962a6f9cb61bcaf91ab4938339
MEASWs <mookvarin.cc1976@gmail.com>
2025-12-15 09:59:21 +0700

    bonus1-2

--- lab3/lab3_no6.cpp
+++ lab3/lab3_no6.cpp
@@ -3,7 +3,16 @@
 int main() {
 
     int score;
+    int need = 0;
     scanf("%d", &score);
+
+    if (score < 0) {
+        printf("error score\n");
+        return 0;
+    } else if (score > 100) {
+        printf("error score\n");
+        return 0;
+    }
 
     if(score < 68){
         if(score < 55){
@@ -12,7 +21,8 @@ int main() {
             printf("d\n");
         }
     }else if(score < 75){
-        printf("c\n");
+        need = 75 - score;
+        printf("c %d\n", need);
     }else if(score < 85){
         printf("b\n");
     }else{
